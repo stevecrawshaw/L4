de2d210c5d4d462981903515bcc3d48a0d57bd36
stevecrawshaw <steve.crawshaw@bristol.gov.uk>
2022-10-27 16:23:50 +0100

    diagnostics2022-10-27

--- project-2-diagnostics/diagnostics-retrieve.R
+++ project-2-diagnostics/diagnostics-retrieve.R
@@ -1,29 +1,33 @@
 library(pacman)
-p <- c("odbc", "tidyverse", "dbplyr", "DBI", "glue", "here", "timetk", "config")
+p <- c("odbc", "dplyr", "DBI", "config", "lubridate")
 p_load(char = p)
 
-Sys.setenv(TZ = "UTC")
+
+connect.envista <- function(){
+    con_params <- get() # credentials
+    
+    #make connection to Envista using details in the config file
+    dbConnect(odbc(),
+                     Driver = con_params$driver,
+                     Server = con_params$server,
+                     Database = con_params$database,
+                     UID = con_params$uid,
+                     PWD = con_params$pwd,
+                     Port = con_params$port
+    ) %>% 
+        return()
+    
+}
 
 
+get.diag.tbl <- function(con, dateon, dateoff){
+# function to get the raw diagnostics table from the envista database
+tbl(con, "TB_DIAGNOSTICS") %>% 
+    filter(between(DIG_DateTime,
+                   as_datetime(dateon),
+                   as_datetime(dateoff))) %>% 
+    collect() %>% 
+        return()
 
-con_params <- get()
+}
 
-#'make connection to Envista using details in the config file
-con <- dbConnect(odbc::odbc(),
-                 Driver = con_params$driver,
-                 Server = con_params$server,
-                 Database = con_params$database,
-                 UID = con_params$uid,
-                 PWD = con_params$pwd,
-                 Port = con_params$port
-)
-
-
-diag_tbl <- tbl(con, "TB_DIAGNOSTICS") %>% 
-    collect()
-
-# diag_tbl %>% 
-#     saveRDS(file = "../data/diag_tbl.rds")
-
-
-con %>% dbDisconnect()
--- project-2-diagnostics/diagnostics-wrangle.R
+++ project-2-diagnostics/diagnostics-wrangle.R
@@ -1,3 +1,4 @@
+# Load packages ----
 packages <-
     c("tidyverse",
       "openair",
@@ -7,58 +8,114 @@ packages <-
 
 pacman::p_load(char = packages)
 
-#tidyverse::tidyverse_packages()
+source("diagnostics-retrieve.R")
+# Variables ----
+dateon <-  "2022-09-01"
+dateoff <-  "2022-10-01"
+month <- month(dateon, label = TRUE,
+               abbr = FALSE) %>%
+    as.character()
+year <- year(dateon)
 
-startDate = "2022-09-01"
-endDate = "2022-09-30"
+# Data ----
+# diagnostics data
+con <- connect.envista()
 
+diag_tbl <- get.diag.tbl(con, dateon, dateoff)
+
+con %>% dbDisconnect()
+
+# site metadata linking station ID's with site ID's and pollutants
 final_tbl <- read_delim(file = "S:/SUSTAIN/Sustain-Common/SCCCS/write_gis_r/R Projects/air_quality_data_management/data/finalContinTablerhtemp.csv", delim = ",", col_types = "ciiciccc")
+# matching siteids to names
+siteid <- c(215L, 270L, 203L, 463L, 501L, 672L)
+site_name <- c("Parson St",
+                "Wells Road",
+                "Brislington",
+                "Fishponds",
+                "Colston Ave",
+                "Marlborough St")
+sites_tbl <- tibble(siteid, site_name)
+# Functions ----
 
-siteids <- c(215, 270, 203, 463, 501, 672)
-
-diag_tbl <- read_rds("../data/diag_tbl.rds")
-
-
-samp_diag_tbl <- diag_tbl %>% 
-    tail(1000)
-
-
-# function to pivot longer the names, units and values from the 
-# diagnostics table and combine into one dataframe
 lengthen.diag <- function(diagnostics_tbl){
-names_df <- diagnostics_tbl %>% 
-    select(DIG_DateTime, DIG_Station, contains("Name")) %>% 
-    pivot_longer(cols = contains("Name"),
-                 names_to = "DIG_Name",
-                 values_to = "parameter")
-
-units_df <- diagnostics_tbl %>% 
-    select(DIG_DateTime, DIG_Station, contains("Unit")) %>% 
-    pivot_longer(cols = contains("Unit"),
-                 names_to = "DIG_Unit",
-                 values_to = "unit")
-
-values_df <- diagnostics_tbl %>% 
-    select(DIG_DateTime, DIG_Station, contains("Value")) %>% 
-    pivot_longer(cols = contains("Value"),
-                 names_to = "DIG_Value",
-                 values_to = "value")
-
-combined_tbl <- cbind(names_df %>% 
-                      select(-DIG_Name),
-                  units_df %>%
-                      select(unit),
-                  values_df %>% 
-                      select(value)) %>% 
-    as_tibble()
-return(combined_tbl)
+    # function to pivot longer the names, units and values from the 
+    # diagnostics table and combine into one dataframe
+    
+    names_df <- diagnostics_tbl %>% 
+        select(DIG_DateTime, DIG_Station, contains("Name")) %>% 
+        pivot_longer(cols = contains("Name"),
+                     names_to = "DIG_Name",
+                     values_to = "parameter")
+    
+    units_df <- diagnostics_tbl %>% 
+        select(DIG_DateTime, DIG_Station, contains("Unit")) %>% 
+        pivot_longer(cols = contains("Unit"),
+                     names_to = "DIG_Unit",
+                     values_to = "unit")
+    
+    values_df <- diagnostics_tbl %>% 
+        select(DIG_DateTime, DIG_Station, contains("Value")) %>% 
+        pivot_longer(cols = contains("Value"),
+                     names_to = "DIG_Value",
+                     values_to = "value")
+    
+    combined_tbl <- cbind(names_df %>% 
+                              select(-DIG_Name),
+                          units_df %>%
+                              select(unit),
+                          values_df %>% 
+                              select(value)) %>% 
+        as_tibble()
+    return(combined_tbl)
 }
+
+clean.plot <- function(data, site, month, year){
+# takes the nested df, strips out conc data (not interesting)
+    # and produced ggplot with facets
+        p <- data %>% 
+        filter(!str_detect(parameter, "Conc")) %>% 
+        ggplot(aes(x = DIG_DateTime, y = value)) +
+        geom_line() +
+        facet_wrap(~parameter + unit, scales = "free_y") +
+        labs(title = glue("Diagnostics plots for {site}: {month} {year}"),
+             x = "Date") +
+        scale_x_datetime(breaks = "1 weeks", date_labels = "%d") +
+        theme_bw() 
+    return(p)
+}
+
+# Wrangling ----
+
+station_site_tbl <- final_tbl %>% 
+    mutate(station = str_remove(tablename, "S") %>% as.integer()) %>% 
+    distinct(station, siteid) %>% 
+    inner_join(sites_tbl, by = "siteid")
 
 combined_tbl <- lengthen.diag(diag_tbl)
 
 # clean it
-combined_tbl %>% 
+clean_combined_tbl <- combined_tbl %>% 
     na.omit() %>% 
     mutate(parameter = str_trim(parameter),
-           unit = str_trim(unit))
+           unit = str_trim(unit)) %>% 
+    inner_join(station_site_tbl, by = c("DIG_Station" = "station")) 
+
+all_sites_plots_tbl <- clean_combined_tbl %>%
+    select(-DIG_Station) %>% 
+    nest_by(siteid, site_name) %>% 
+    mutate(plot = list(clean.plot(data,
+                                  site = site_name,
+                                  {{month}},
+                                  {{year}})))
+
+#testing plotting
+# clean.plot(bris_test_tbl, site ="bris", month, year)
+# 
+# plot_tbl <- all_sites_tbl %>% 
+
+
+plot_tbl$plot[3]
+
+
 
