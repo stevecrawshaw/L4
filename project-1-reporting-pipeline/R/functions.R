8c0e0822026f7c53d28e786f318fc0bf513ce77f
stevecrawshaw <steve.crawshaw@bristol.gov.uk>
2022-11-04 14:39:37 +0000

    pipeline-functions-targets-1

--- project-1-reporting-pipeline/R/functions.R
+++ project-1-reporting-pipeline/R/functions.R
@@ -10,10 +10,17 @@ p <-
       "openair",
       "fastverse",
       "janitor",
-      "timetk")
+      "timetk",
+      "glue",
+      "lubridate")
 p_load(char = p)
 
+startDate <- "2021-01-01"
+endDate <- "2021-12-31"
+
 # Functions ----
+
+# Load data ----
 
 connect.envista <- function() {
     con_params <- get(config = "envista") # credentials
@@ -217,7 +224,7 @@ multi.site.year.import <- function(sites, years) {
         return()
 }
 
-import.aurn.uk-air <- function(sites, startDate, endDate){
+import.aurn.uk.air <- function(sites, startDate, endDate){
 
     years <- unique(year(c(as.Date(startDate), as.Date(endDate))))
 
@@ -229,7 +236,6 @@ aurndata <- multi.site.year.import(sites, years) %>%
     select(date, siteid,
            nox, no2, no, pm10, pm2.5, o3, -site)
 }
-
 
 get.aurn.openair <- function(sites = c("BRS8", "BR11"),
                      startDate,
@@ -259,4 +265,187 @@ get.aurn.openair <- function(sites = c("BRS8", "BR11"),
                -c(site, ws, wd)) %>%
         return()
     
+}
+
+connect.access <- function(){
+    con <- dbConnect(odbc::odbc(), .connection_string = "Driver={Microsoft Access Driver (*.mdb, *.accdb)}; Dbq=S:\\SUSTAIN\\Sustain-Common\\SCCCS\\write_gis_r\\tube_database\\access_2010_versions\\no2_data.accdb;")
+    return(con)
+    
+}
+con <- connect.access()
+get.no2.data.db <- function(con, startDate, endDate){
+    # get the raw no2 data for the year
+    safe_start <- as.Date(startDate) - weeks(6)
+    safe_end <- as.Date(endDate) + weeks(6)
+    year <- year(as.Date(startDate))
+    df <- tbl(con, "data") %>% 
+        filter(dateOn > safe_start,
+               dateOff < safe_end) %>% 
+        collect()
+    
+    out <- df %>% 
+        mutate(mid_date = ((dateOff - dateOn) / 2) + dateOn) %>% 
+        filter(year(mid_date)  == year) %>% 
+        select(dateon = dateOn,
+               mid_date,
+               dateoff = dateOff,
+               siteid = LocID,
+               concentration,
+               id = ID)
+    return(out)
+    
+}
+
+out_df <- get.no2.data.db(con, startDate, endDate)
+
+get.background.data <- function(no2_data){
+
+    year <- year(no2_data$mid_date %>% median(na.rm = TRUE))
+    mindate = no2_data$dateOn %>% min(na.rm = TRUE)
+    maxdate = no2_data$dateOff %>% max(na.rm = TRUE)
+contin_back_sites <- importMeta(all = T)  %>% 
+    filter(between(latitude, 50.70, 52.24), 
+           between(longitude, -3.79, -1.42),
+           site_type %in% c("Urban Background"),
+           Parameter_name == "Nitrogen dioxide",
+           end_date == "ongoing")
+
+years_vec <- seq(from = as.integer(year) - 1, to = as.integer(year) + 1, by = 1)
+#get the data
+back_sites <- c("BRS8", "BORN", "NPT3", "SWHO") # MANUAL
+
+back_site_names <- contin_back_sites %>% 
+    filter(code %in% back_sites) %>% 
+    select(site)
+
+annual_back_data <- importAURN(site = back_sites,
+                               pollutant = "no2",
+                               year = years_vec) %>%
+    with_tz(tzone = 'UTC') %>%
+    filter(date %>%
+               between(with_tz(as.POSIXct(mindate), 'UTC'),
+                       with_tz(as.POSIXct(maxdate + 1), 'UTC')))
+return(annual_back_data)
+
+}
+
+get.aqms <- function(con){
+    instrument_tbl <- tibble::tribble(
+        ~instrumenttype, ~instrument_id,
+        "Diffusion Tube", 1L,
+        "Continuous (Reference)", 2L,
+        "Volume Sampler", 3L,
+        "Frisbee", 4L,
+        "Anemometer", 5L,
+        "Thermometer", 6L,
+        "Rain Gauge", 7L,
+        "Hygrometer", 8L,
+        "Radiometer", 9L,
+        "Continuous (Indicative)", 10L
+    )
+        #    get air quality monitoring sites from access database
+    aqms_tbl <- tbl(con, 'locations') %>%
+        filter(!is.na(Easting), !is.na(Northing)) %>% 
+        filter(!SiteID == 573L) %>% 
+        collect() %>% 
+        clean_names() %>% 
+        left_join(instrument_tbl, by = "instrument_id") %>% 
+        rename(sample_height = tube_height,
+               siteid = site_id) %>% 
+        select(-site_no, -instrument_id, route_id)
+    
+    return(aqms_tbl)
+}
+
+# Wrangle data ----
+
+pivot.tubes.monthly <- function(no2_data){
+    #construct the wide format concentration data grouped by siteid and month
+    no2_data %>% 
+        mutate(month_num = lubridate::month(mid_date) %>% as.integer(),
+               month = month.abb[month_num] %>%
+                   as_factor(),
+               siteid = as.integer(siteid)) %>%
+        group_by(month, siteid)  %>% 
+        mutate(row = row_number()) %>%
+        ungroup() %>% 
+        arrange(month_num) %>% 
+        pivot_wider(id_cols = c(siteid, row),
+                    names_from = month,
+                    values_from = concentration) %>%
+        arrange(siteid, row) %>% 
+        select(-row) %>%
+        return()
+}
+
+pivoted_tubes <- pivot.tubes.monthly(out_df)
+
+aq_monitors <- get.aqms(con)
+
+step_2_table <- get.aqms(con) %>% 
+    filter(instrumenttype == "Diffusion Tube") %>% 
+    transmute(siteid,
+              location,
+              dup_trip = if_else(!is.na(duplicate_triplicate),
+                                 glue("{siteid}_{duplicate_triplicate}"),
+                                 NA_character_),
+              easting, northing, laqm_locationclass,
+              dist_exposure = if_else(exposure,
+                                      rec_kerb_distance_m - tube_kerb_distance_m,
+                                      0),
+              # if there's no exposure make distances 0 so that the
+              # spreadsheet doesn't run a fall off distance calc
+              tube_kerb_distance = tube_kerb_distance_m) %>% 
+    right_join(       , by = c("siteid" = "siteid")) %>% 
+    left_join(last_years_sites, by = c("siteid" = "siteid")) %>% 
+    mutate(new_existing = if_else(is.na(exist_last_year), "New", "Existing")) %>%
+    group_by(siteid, dup_trip) %>%
+    mutate(row_id = row_number()) %>%
+    ungroup() %>%
+    mutate(newsiteid = if_else(!is.na(dup_trip),
+                               glue("{as.character(siteid)}_{row_id}"),
+                               glue("{siteid}")),
+           .before = siteid) %>%
+    arrange(siteid) %>% 
+    select(newsiteid:dup_trip, new_existing,
+           everything(),
+           -exist_last_year,
+           -siteid,
+           -row_id)
+
+
+connect.access() %>% 
+    get.no2.data.db(startDate, endDate) %>% 
+    pivot.tubes.monthly()
+
+dbDisconnect(con)
+
+make.step2.table <- function(aqms_tbl){
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+# write data ----
+
+write.background.data <- function(annual_background_data){
+
+annual_background_data %>% 
+    select(-code) %>% 
+    pivot_wider(id_cols = date, names_from = site, values_from  = no2) %>% 
+    arrange(date) %>% 
+
+    write_delim(file = glue("data/{year}_step_2a_annualisation_inputs.csv"), delim = ",", na = "")
+
 }
