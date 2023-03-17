f102bd452c5c4e5fb07a1b8bf98dd77a2a5b94a3
Steve Crawshaw <steve.crawshaw@bristol.gov.uk>
2023-03-17 15:57:56 +0000

    model selection

--- project-4-colocation-study/R/functions.R
+++ project-4-colocation-study/R/functions.R
@@ -578,11 +578,11 @@ make.model.perf.tbl.gt <- function(model_output_tbl){
     
 }
 
-save.model.perf.tbl.gt <- function(model_perf_tbl_gt){
+save.model.perf.tbl.gt <- function(model_perf_tbl_gt, filename = "model_gt_name"){
     
-    filename = "plots/model_perf_tbl_gt"
-    fhtml <- glue("{filename}.html")
-    fpng <- glue("{filename}.png")
+    path <- glue("plots/{filename}")
+    fhtml <- glue("{path}.html")
+    fpng <- glue("{path}.png")
         
     model_perf_tbl_gt %>% 
         tbl_butcher() %>% 
--- project-4-colocation-study/function_testing.R
+++ project-4-colocation-study/function_testing.R
@@ -52,7 +52,7 @@ model_data_tbl %>%
 
 model_data_tbl <- read_rds(file = "data/model_data_tbl.rds")
 
-plot.drift.site.gg(model_data_tbl, site = 500)
+plot.drift.site.gg(model_data_tbl, site = 215)
 
 plot.scatter.site.gg(model_data_tbl$md_wide[1][[1]])
 plot.scatter.site.gg(model_data_tbl$md_wide[2][[1]])
@@ -64,6 +64,132 @@ tpgg <- plot.time.series.gg(timeplot_tbl, interval = "day")
 sp_plot_tbl <- prep.sp.plot.tbl(timeplot_tbl)
 
 save.png.summaryplot(sp_plot_tbl, pollutant = "pm10")
+
+md_wide_split <- model_data_tbl$md_wide[1][[1]] %>% 
+    na.omit() %>% 
+    initial_time_split()
+
+model.parameter.test <- function(md_wide_split){
+# this function tests 3 combinations of input paramaters for a lm
+    # and outputs a tbl of performance metrics for each one
+    
+    # the models are run on the training data and tested on the test data
+    # as defined by the rsample::initial time splits function
+trained_model_reference_low_cost <- linear_reg() %>% 
+    set_engine('lm') %>% 
+    fit(reference ~ low_cost,
+        data = training(md_wide_split))
+
+trained_model_reference_low_cost_humidity <- linear_reg() %>% 
+    set_engine('lm') %>% 
+    fit(reference ~ low_cost + humidity,
+        data = training(md_wide_split))
+
+trained_model_reference_low_cost_humidity_temperature <- linear_reg() %>% 
+    set_engine('lm') %>% 
+    fit(reference ~ low_cost + humidity + temperature,
+        data = training(md_wide_split))
+
+model_test_list <- list("reference_lowcost" = trained_model_reference_low_cost,
+                        "reference_lowcost_humidity" = trained_model_reference_low_cost_humidity,
+                        "reference_lowcost_humidity_temperature" = trained_model_reference_low_cost_humidity_temperature)
+
+metric.test <- function(trained_model, md_wide_split){
+    pred_tbl <- augment(trained_model, new_data = testing(md_wide_split))
+    pred_tbl %>% 
+    model_metrics(truth = reference, estimate = .pred) %>% 
+        return()
+}
+
+
+map_dfr(model_test_list, ~metric.test(.x,
+                                      md_wide_split = md_wide_split),
+        .id = "model") %>% 
+    transmute(model = str_replace_all(model, "_", " "),
+              .estimator = NULL, .metric, .estimate) %>% 
+    pivot_wider(id_cols = model, names_from = .metric, values_from = .estimate) %>% 
+    return()
+}
+
+make.model.select.tbl <- function(model_data_tbl){
+
+model_select_tbl <- model_data_tbl %>% 
+    select(md_wide) %>% 
+    mutate(md_wide_split = md_wide %>%
+               pluck(1) %>%
+               na.omit() %>% 
+               initial_time_split() %>%
+               list(),
+           parameter_test_results = 
+               model.parameter.test(md_wide_split = pluck(md_wide_split,1)) %>%
+               list())
+
+return(model_select_tbl)
+}
+
+model_select_tbl <- make.model.select.tbl(model_data_tbl)
+
+make.model.select.gt <- function(model_select_tbl){
+
+model_select_gt <- model_select_tbl %>% 
+    select(-md_wide_split, -md_wide) %>% 
+    unnest(parameter_test_results) %>% 
+    mutate(pollutant = if_else(siteid == 215L,
+                               md("PM<sub>2.5</sub>"),
+                               md("PM<sub>10</sub>")),
+           .after = siteid) %>% 
+    
+    gt() %>% 
+    cols_label(pollutant = "Pollutant",
+               model = "Model terms",
+               rmse = "RMSE",
+               rsq = "R squared",
+               mae = "MAE") %>% 
+    fmt_markdown(columns = c("pollutant")) %>% 
+    fmt_number(columns = c("rmse", "rsq", "mae"),
+               decimals = 3) %>% 
+    tab_header("Model Selection Metrics")
+
+return(model_select_gt)
+
+}
+
+model_select_gt <- make.model.select.gt(model_select_tbl = model_select_tbl)
+
+gtsave(model_select_gt, 'plots/model_select_gt.png')
+
+
+
+check_model(trained_model)    
+tidy(trained_model)
+glance(trained_model)
+
+pred_tbl <- augment(trained_model, new_data = testing(md_wide_split)) 
+
+pred_tbl %>% 
+    select(date, reference, .pred) %>% 
+    pivot_longer(cols = -date) %>% 
+    ggplot(aes(x = date, y = value, colour = name)) +
+    geom_line()
+
+model_metrics <- metric_set(yardstick::rmse,
+                            yardstick::rsq,
+                            yardstick::mae)
+
+pred_tbl %>% 
+    model_metrics(truth = reference, estimate = .pred)
+
+
+
+training(md_wide_split)
+
+
+
+
+
+testing(md_wide_split)
+
+
 model_output_tbl <- make.model.output.tbl(model_data_tbl)
 
 scatter_gg_215 <- model_output_tbl[model_output_tbl$siteid == 215,]$plot[[1]]
@@ -76,6 +202,5 @@ model_data_tbl <- read_rds("data/model_data_tbl.rds")
 model_perf_tbl <- make.model.perf.tbl(model_output_tbl)
 
 model_perf_tbl_gt <- make.model.perf.tbl.gt(model_output_tbl)
-
 
 
